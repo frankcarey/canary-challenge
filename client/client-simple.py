3d40b8e5960b6df0e189ca032dd7d5a72fa08ff5
Frank Carey <fcarey@gmail.com>
2013-08-30 12:35:09 -0400

    Add redis pubsub functionality to server-simple. For some reason, a second device cannot register. I'm guessing that redis.pubsub.listen() blocks, even though it's on the accept handler, which I thought would be running in it's own thread. Maybe asyncore doesn't use threading??

--- client/client-simple.py
+++ client/client-simple.py
@@ -4,8 +4,11 @@ s.connect(('127.0.0.1', 8888))
 
 serial = sys.argv[1]
 
-while(1):
-    register_msg = json.dumps({'device':serial})
-    print register_msg
-    s.send(register_msg + "\r\n")
-    time.sleep(3)
+register_msg = json.dumps({'device':serial})
+print register_msg
+s.send(register_msg + "\r\n")
+status =  s.recv(8192)
+if status == 'OK':
+    while (1):
+        update = s.recv(8192)
+        print str(update)
--- server/server-asyncore.py
+++ server/server-asyncore.py
@@ -1,13 +1,31 @@
-import asyncore
+import asyncore, time, json
 import socket
+import redis
+
+
+
+devices = {}
+notification_queue = []
 
 class EchoHandler(asyncore.dispatcher_with_send):
+    redis = redis.Redis().pubsub()
 
     def handle_read(self):
         data = self.recv(8192)
-        if data:
-            print repr(self.addr) + str(data)
-
+        data = json.loads(data)
+        if data['device'] :
+            devices[data['device']] = self
+            print 'Device Registered:' + data['device']
+            self.redis.subscribe('device.' + data['device'])
+            self.send('OK')
+            # Use psubscribe when using pattern matching.
+            for data_raw in self.redis.listen():
+                data = data_raw['data']
+                print data
+                self.send(json.dumps(data))
+            #self.send(json.dumps({'privacyMode': True}))
+    def handle_publish(self, msg):
+        print msg
 class EchoServer(asyncore.dispatcher):
 
     def __init__(self, host, port):
@@ -22,8 +40,11 @@ class EchoServer(asyncore.dispatcher):
         if pair is not None:
             sock, addr = pair
             self._address = addr
+            #Add the address to the overall connection list.
             print 'Incoming connection from %s' % repr(addr)
+            print 'Current connections'
             handler = EchoHandler(sock)
 
 server = EchoServer('localhost', 8888)
+print 'Listening on ' + str(server.addr)
 asyncore.loop()
