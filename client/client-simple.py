51d1f45c2a4dccfcfd91687e996159635923296a
Frank Carey <fcarey@gmail.com>
2013-08-30 16:17:11 -0400

    A Threaded version of the client. It puts the realtime client in a thread, but that doesn't seem to nicely handle ctl-c (there is no graceful thread.stop(). FINALLY going to look at gevent to see if that makes more sense. Uses greenlets to do async instead of threads.

--- client/client-simple.py
+++ client/client-simple.py
@@ -1,24 +1,30 @@
-import sys, time, socket, json
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
-s.connect(('127.0.0.1', 8888))
+import sys, time, socket, json, threading
 
-serial = sys.argv[1]
 privacyMode = False
+serial = sys.argv[1]
 
-register_msg = json.dumps({'device':serial})
-print 'Sent: ' + register_msg
-print 'Attempting to register....'
-s.send(register_msg + "\r\n")
-msg =  s.recv(8192)
-if msg == 'OK\r\n':
-    print 'Device Registered'
-    while (1):
-        print 'Listening for updates...'
-        msg = s.recv(8192)
-        print 'Received: ' + str(msg)
-        msg_dict = json.loads(msg)
-        if (msg_dict['privacyMode']):
-            privacyMode = msg_dict['privacyMode']
-            print 'Updated privacyMode to: ' + str(msg_dict['privacyMode'])
-else:
-    print 'Unexpected Message: ' + msg
+
+def realtime_client_run():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+    s.connect(('127.0.0.1', 8888))
+
+    register_msg = json.dumps({'device':serial})
+    print 'Sent: ' + register_msg
+    print 'Attempting to register....'
+    s.send(register_msg + "\r\n")
+    msg =  s.recv(8192)
+    if msg == 'OK\r\n':
+        print 'Device Registered'
+        while (1):
+            print 'Listening for updates...'
+            msg = s.recv(8192)
+            print 'Received: ' + str(msg)
+            msg_dict = json.loads(msg)
+            if (msg_dict['privacyMode']):
+                    privacyMode = msg_dict['privacyMode']
+                    print 'Updated privacyMode to: ' + str(msg_dict['privacyMode'])
+        else:
+            print 'Unexpected Message: ' + msg
+
+realtime_client = threading.Thread(target=realtime_client_run)
+realtime_client.start()
