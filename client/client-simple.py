f775b5291b2b2d3d3b1fe3f6e457333b684d6911
Frank Carey <fcarey@gmail.com>
2013-08-30 17:41:42 -0400

    Update client-example.py so that it can be imported.

--- client/client-simple.py
+++ client/client-simple.py
@@ -1,30 +1,36 @@
-import sys, time, socket, json, threading
-
-privacyMode = False
-serial = sys.argv[1]
-
+import sys, time, socket, json
 
 def realtime_client_run():
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
-    s.connect(('127.0.0.1', 8888))
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+        s.connect(('127.0.0.1', 8888))
 
-    register_msg = json.dumps({'device':serial})
-    print 'Sent: ' + register_msg
-    print 'Attempting to register....'
-    s.send(register_msg + "\r\n")
-    msg =  s.recv(8192)
-    if msg == 'OK\r\n':
-        print 'Device Registered'
-        while (1):
-            print 'Listening for updates...'
-            msg = s.recv(8192)
-            print 'Received: ' + str(msg)
-            msg_dict = json.loads(msg)
-            if (msg_dict['privacyMode']):
-                    privacyMode = msg_dict['privacyMode']
-                    print 'Updated privacyMode to: ' + str(msg_dict['privacyMode'])
-        else:
-            print 'Unexpected Message: ' + msg
-
-realtime_client = threading.Thread(target=realtime_client_run)
-realtime_client.start()
+        register_msg = json.dumps({'device':serial})
+        print 'Sent: ' + register_msg
+        print 'Attempting to register....'
+        s.send(register_msg + "\r\n")
+        msg =  s.recv(8192)
+        if msg == 'OK\r\n':
+            print 'Device Registered'
+            while (1):
+                print 'Listening for updates...'
+                msg = s.recv(8192)
+                print 'Received: ' + str(msg)
+                msg_dict = json.loads(msg)
+                if (msg_dict['privacyMode']):
+                        privacyMode = msg_dict['privacyMode']
+                        print 'Updated privacyMode to: ' + str(msg_dict['privacyMode'])
+            else:
+                print 'Unexpected Message: ' + msg
+    except socket.error, e:
+        return e
+# Start the server
+def main(args):
+    try:
+        serial = args[0]
+        privacyMode = False
+        print realtime_client_run()
+    except KeyboardInterrupt:
+        pass
+if __name__ == '__main__':
+    main(sys.argv[1:])
